992666d1697f5e998ecaf908964c327c02cefbb6
KrasnovaYana <alsopug@gmail.com>
2026-09-09 17:28:48 +0500

    Task5

---
+++ Tyuiu.KrasnovaYA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.KrasnovaYA.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.KrasnovaYA.Sprint0.Task5.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+
+        public void CheckedSubstractinValid()
+        {
+            Assert.AreEqual(5, DataService.Substraction(10, 5));
+        }
+        [TestMethod]
+
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
