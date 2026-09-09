41cedfd97ce6c3051cb4e07649d904937adaf2b8
KrasnovaYana <alsopug@gmail.com>
2026-09-09 18:03:59 +0500

    Task6

---
+++ Tyuiu.KrasnovaYA.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,6 @@
+﻿using Tyuiu.KrasnovaYA.Sprint0.Task6.V0.Lib;
+int[] numArray = new int[] { 1, 2, 3, 4, 5 };
+Console.WriteLine("Сумма элементов массива" + DataService.AdditionArray(numArray));
+Console.WriteLine("Разность элементов массива" + DataService.SubtractionArray(numArray));
+Console.WriteLine("Произведение элементов массива" + DataService.MultiplicationArray(numArray));
+Console.ReadKey();
